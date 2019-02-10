9d90cff6bdf1068724cb6ddc5e9672ae9b2d6da3
Krystian z <krystian.zawistowski@zoho.com>
2019-02-10 18:00:50 +0100

    debug_info

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Dectree',
-      version='1.0',
+      version='1.0.01',
       description='decision tree',
       author='Krystian',
       packages=['dectree']
